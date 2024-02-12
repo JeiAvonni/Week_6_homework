0aa8cf5a71c4397a4e7cb4f9525b31c728843402
jeiavonni <jeiavonni765@gmail.com>
2024-02-12 08:35:22 -0600

    Added tables and minor debugging

--- target_shop/models.py
+++ target_shop/models.py
@@ -1,16 +1,13 @@
-# Password hash for extra security
+from msilib.schema import SelfReg
+from typing import Self
 from werkzeug.security import generate_password_hash
-# ORM (Object Relational Mapper)
 from flask_sqlalchemy import SQLAlchemy 
-# Loading current_user
 from flask_login import UserMixin, LoginManager 
-# Data timestamp
 from datetime import datetime 
-# Unique id for data
 import uuid 
-
 from flask_marshmallow import Marshmallow
 
+# Internal imports
 
 #Instantiate all our classes
 
@@ -53,8 +50,6 @@ class User(db.Model, UserMixin):
         self.email = email 
         self.password = self.set_password(password) 
 
-
-
     # Methods for editting attributes 
     def set_id(self):
         return str(uuid.uuid4())
@@ -71,6 +66,36 @@ class User(db.Model, UserMixin):
     def __repr__(self):
         return f"<User: {self.username}>"
     
+
+class ProdOrder(db.Model):
+    prodorder_id = db.Column(db.String, primary_key=True)
+    prod_id = db.Column(db.String, db.ForeignKey('product.prod_id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable = False)
+    price = db.Column(db.Numeric(precision = 10, scale = 2), nullable = False)
+    order_id = db.Column(db.String, db.ForeignKey('order.order_id'), nullable = False)
+    cust_id = db.Column(db.String, db.ForeignKey('customer.cust_id'), nullable = False)
+
+def __init__(self, prod_id,quantity, price, order_id, cust_id):
+    self.prodorder_id = self.set_id()
+    self.prod_id = prod_id
+    self.quantity = quantity
+    self.price = self.set_price(quantity, price)
+    self.order_id = order_id
+    self.cust_id = cust_id
+
+def set_id(self):
+    return str(uuid.uuid4())
+
+def set_price(self,quantity, price):
+    quantity = int(quantity)
+    price = float(price)
+    self.price = quantity + price
+    return self.price
+
+def update_quantity(self,quantity):
+    self.quantity = quantity
+
+
 class Product(db.Model): 
     prod_id = db.Column(db.String, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
@@ -79,6 +104,7 @@ class Product(db.Model):
     price = db.Column(db.Numeric(precision=10, scale=2), nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
     date_added = db.Column(db.DateTime, default = datetime.utcnow)
+    ProdOrder = db.relationship('ProdOrder', backref = 'product', lazy=True)
   
 
     def __init__(self, name, price, quantity, image="", description=""):
@@ -114,6 +140,55 @@ class Product(db.Model):
 
     def __repr__(self):
         return f"<Product: {self.name}>"
+    
+class Customer(db.Model):
+    cust_id = db.Column(db.String, primary_key=True)
+    date_created = db.Column(db.DateTime, default = datetime.utcnow())
+    prodord = db.relationship('ProdOrder', backref = 'customer', lazy=True) 
+
+    def __init__(self, cust_id):
+        self.cust_id = cust_id 
+
+
+    def __repr__(self):
+        return f"<Customer: {self.cust_id}>"
+    
+class Order(db.Model):
+    order_id = db.Column(db.String, primary_key=True)
+    order_total = db.Column(db.Numeric(precision=10, scale=2), nullable = False)
+    date_created = db.Column(db.DateTime, default = datetime.utcnow())
+    prodord = db.relationship('ProdOrder', backref = 'order', lazy=True)
+
+
+
+    def __init__(self):
+        self.order_id = self.set_id()
+        self.order_total = 0.00
+
+    
+    def set_id(self):
+        return str(uuid.uuid4())
+    
+
+    def increment_ordertotal(self, price):
+
+        self.order_total = float(self.order_total) 
+        self.order_total += float(price)
+
+        return self.order_total
+    
+    def decrement_ordertotal(self, price):
+
+        self.order_total = float(self.order_total)
+        self.order_total -= float(price)
+
+        return self.order_total
+    
+
+    def __repr__(self):
+        return f"<Order: {self.order_id}>"
+
+
 
 
 class ProductSchema(ma.Schema):
